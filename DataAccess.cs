7925f4b28287b8aefe90aa6f9180eebd33bf21f9
Annika Dietz <annika.dietz@student.stenden.com>
2020-03-31 02:53:54 +0200

    Adding the correct database information

--- DataAccess.cs
+++ DataAccess.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
-using System.Collections.Generic;
 using Windows.Storage;
 using System.IO;
+using System.Diagnostics;
 
 namespace MonopolyAnalysis
 {
@@ -14,26 +14,115 @@ namespace MonopolyAnalysis
     {
         public async static void InitializeDatabase()
         {
-            await ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", CreationCollisionOption.OpenIfExists);
-            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("monopolyDatabase.db", CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "monopolyDatabase.db");
             using (SqliteConnection db =
                new SqliteConnection($"Filename={dbpath}"))
             {
                 db.Open();
 
-                String tableCommand = "CREATE TABLE IF NOT " +
-                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
-                    "Text_Entry NVARCHAR(2048) NULL)";
+                String gameMoveTable = "CREATE TABLE IF NOT EXISTS GameMove ( " +
+                      "GameMoveID int PRIMARY KEY," +
+                      "PlayerID int, " +
+                      "GameFieldLandedOnDiceRoll int, " +
+                      "GameID int, " +
+                      "MoneySpend double, " +
 
-                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                      "CONSTRAINT FK_PlayerID FOREIGN KEY(PlayerID) " +
+                      "REFERENCES Player(PlayerID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE, " +
 
-                createTable.ExecuteReader();
+                      "CONSTRAINT FK_GameFieldLandedOnDiceRoll FOREIGN KEY(GameFieldLandedOnDiceRoll) " +
+                      "REFERENCES GameField(GameFieldID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE, " +
+
+                      "CONSTRAINT FK_GameID FOREIGN KEY(GameID) " +
+                      "REFERENCES Game(GameID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE " +
+                      ");";
+
+                String playerTable = "CREATE TABLE IF NOT EXISTS Player ( " +
+                      "PlayerID int PRIMARY KEY, " +
+                      "GameID int, " +
+                      "FinalTotalMoney decimal, " +
+
+                      "CONSTRAINT FK_GameID FOREIGN KEY(GameID) " +
+                      "REFERENCES Game(GameID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE " +
+                      "); ";
+
+                String gameTable = "CREATE TABLE IF NOT EXISTS Game ( " +
+                      "GameID INTEGER PRIMARY KEY " +
+                      ") ";
+               
+
+                String playerGameFieldsTable = "CREATE TABLE IF NOT EXISTS PlayerGameFields ( " +
+                      "PlayerGameFieldsID int PRIMARY KEY, " +
+                      "GameFieldID int, " +
+                      "PlayerID int, " + 
+
+                      "CONSTRAINT FK_GameFieldID FOREIGN KEY(GameFieldID) " +
+                      "REFERENCES GameField(GameFieldID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE, " +
+
+                      "CONSTRAINT FK_PlayerID FOREIGN KEY(PlayerID) " +
+                      "REFERENCES Player(PlayerID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE " + 
+                      "); ";
+
+                String gameFieldTable = "CREATE TABLE IF NOT EXISTS GameField ( " +
+                      "GameFieldID int PRIMARY KEY, " +
+                      "FieldName text, " +
+                      "IsJailed int, " +
+                      "FieldGroupID int, " +
+                      "PlayerID int, " +
+
+                      "CONSTRAINT FK_PlayerID FOREIGN KEY(PlayerID) " +
+                      "REFERENCES Player(PlayerID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE, " +
+
+                      "CONSTRAINT FK_FieldGroupID FOREIGN KEY(FieldGroupID) " +
+                      "REFERENCES FieldGroup(FieldGroupID) " +
+                      "ON UPDATE CASCADE " +
+                      "ON DELETE CASCADE " +
+                      "); ";
+
+                String fieldGroupTable = "CREATE TABLE IF NOT EXISTS FieldGroup ( " +
+                      "FieldGroupID int PRIMARY KEY, " +
+                      "Color text UNIQUE" +
+                      "); ";
+
+
+                List<String> commandStrings = new List<string>
+                {
+                    gameTable,
+                    playerTable,
+                    fieldGroupTable,
+                    gameFieldTable,
+                    playerGameFieldsTable,
+                    gameMoveTable
+                };
+                
+                foreach (string tableCommand in commandStrings)
+                {
+                    Debug.WriteLine(tableCommand);
+                    SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                    createTable.ExecuteReader();
+                } 
             }
         }
 
-        public static void AddData(string inputText)
+        public static void AddFieldGroupColor(string color)
         {
-            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "monopolyDatabase.db");
             using (SqliteConnection db =
               new SqliteConnection($"Filename={dbpath}"))
             {
@@ -43,12 +132,24 @@ namespace MonopolyAnalysis
                 insertCommand.Connection = db;
 
                 // Use parameterized query to prevent SQL injection attacks
-                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
-                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+                insertCommand.CommandText = "INSERT INTO FieldGroup VALUES (NULL, @Color);";
+                insertCommand.Parameters.AddWithValue("@Color", color);
 
-                insertCommand.ExecuteReader();
-
-                db.Close();
+                try
+                {
+                    insertCommand.ExecuteReader();
+                }
+                catch (SqliteException ex)
+                {
+                    Debug.WriteLine("Inner Exception: " + ex.Message);
+                    Debug.WriteLine("");
+                    Debug.WriteLine("Query Executed: " + insertCommand.CommandText);
+                    Debug.WriteLine("");
+                }
+                finally
+                {
+                    db.Close();
+                }
             }
 
         }
@@ -57,14 +158,14 @@ namespace MonopolyAnalysis
         {
             List<String> entries = new List<string>();
 
-            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "monopolyDatabase.db");
             using (SqliteConnection db =
                new SqliteConnection($"Filename={dbpath}"))
             {
                 db.Open();
 
                 SqliteCommand selectCommand = new SqliteCommand
-                    ("SELECT Text_Entry from MyTable", db);
+                    ("SELECT Color from FieldGroup", db);
 
                 SqliteDataReader query = selectCommand.ExecuteReader();
 
