ada67fc58b83eff19c1e99f34166ebbe2cd221dc
ramimazloum871 <37105837+ramimazloum871@users.noreply.github.com>
2020-04-01 14:31:10 +0200

    write the description of the application in the About page.
    write the description of the application in the About page.

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -36,5 +36,15 @@ namespace MonopolyAnalysis
         {
             this.Frame.Navigate(typeof(SimulationPage), null, new Windows.UI.Xaml.Media.Animation.SuppressNavigationTransitionInfo());
         }
+
+        private void title_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
